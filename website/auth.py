df8623c906db4b2c532ecd545cd08c1219a094b3
jackmaple2 <jackmaple3000@gmail.com>
2023-11-01 18:10:41 +0000

    adds base, home, login and signup template setup

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return '<p>login</p>'
+    return render_template('login.html', boolean=True, user=current_user)
 
 @auth.route('/logout')
 def logout():
@@ -12,4 +13,4 @@ def logout():
 
 @auth.route('/signup')
 def signup():
-    return '<p>signup</p>'
+    return render_template('signup.html', user=current_user)
