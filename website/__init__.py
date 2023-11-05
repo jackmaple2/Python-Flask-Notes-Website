36a017d6e082ccc64fff3aa58335ec29be1b0524
jackmaple2 <jackmaple3000@gmail.com>
2023-11-05 01:34:29 +0000

    fixes database creation syntax error

--- website/__init__.py
+++ website/__init__.py
@@ -17,14 +17,9 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
-
-    create_database(app)
-
-
     return app
 
-def create_database(app):
+def create_database():
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
+        db.create_all()
         print('Created Database!')
